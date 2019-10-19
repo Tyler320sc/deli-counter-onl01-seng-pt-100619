0dbe75b438ed6946f008c3662db21f26c175d665
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-19 03:17:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,16 @@
 
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli == 0 
+    puts "The line is currently empty."
+  else 
+    name_with_numbers = []
+    katz_deli.each.with_index do |name, index|
+      name_with_numbers.push("#{index + 1}. #{name}")
+    end 
+    puts "The line is currently: #{name_with_numbers.join(" ")}"
+end
 
 
 
