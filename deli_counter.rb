78c46a246da421a9c2c25a39213027971cb28960
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-19 03:20:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else 
     name_with_numbers = []
-    katz_deli.each.with_index do |name, index|
+    katz_deli.each.with_index do |name,index|
       name_with_numbers.push("#{index + 1}. #{name}")
     end 
     puts "The line is currently: #{name_with_numbers.join(" ")}"
