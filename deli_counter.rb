a571cb79058d12a7b66391d622f6408e0b293e58
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-19 03:03:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,60 @@
 # Write your code here.
+
+def line(kats_deli)
+  deli_line = []
+  if katz_deli.empty?
+    # current_line = "The line is currently:"
+    kats_deli.each_with_index do |person, i|
+      deli_line.push("1. Logan 2. Avi 3. Spencer")
+    end
+  end
+
+
+
+# else    
+#       '''''''''you start in here
+#         current_line = "The line is currently:"
+
+#       `````````now find a way to add to that string current_line
+#   end
+
+
+
+
+
+
+# let(:katz_deli) { [] }
+
+# line(katz_deli)
+
+#   context "there are people in line" do
+#       it "should display the current line" do
+#         expect($stdout).to receive(:puts).with("The line is currently: 1. Logan 2. Avi 3. Spencer")
+#         line(other_deli)
+#       end
+
+#       it 'works with different people in line' do
+# 	      expect($stdout).to receive(:puts).with("The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey")
+# 	line(another deli)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
